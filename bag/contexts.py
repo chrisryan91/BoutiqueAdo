737afc459a21e6c8878fe110845c3cd474241f30
Chris <christopherryan91@gmail.com>
2024-01-28 19:13:27 +0000

    Add adjust bag and remove from bag views, and calc subtotal template filter

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,13 +3,12 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-
 def bag_contents(request):
 
     bag_items = []
     total = 0
     product_count = 0
-    bag = request.session.get("bag", {})
+    bag = request.session.get('bag', {})
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
@@ -34,22 +33,22 @@ def bag_contents(request):
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
-        "bag_items": bag_items,
-        "total": total,
-        "product_count": product_count,
-        "delivery": delivery,
-        "free_delivery_delta": free_delivery_delta,
-        "free_delivery_threshold": settings.FREE_DELIVERY_THRESHOLD,
-        "grand_total": grand_total,
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
     }
 
     return context
