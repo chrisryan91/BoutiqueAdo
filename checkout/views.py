d1e6105b4e9e05c5821d5af938f7b27cef25b535
Chris <christopherryan91@gmail.com>
2024-02-04 12:28:27 +0000

    Add stripe functionality to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,24 +1,45 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
-from .forms import OrderForm
-from products.models import Product
-from .models import OrderLineItem, Order
-from bag.contexts import bag_contents
-import stripe
-from dotenv import load_dotenv
-import os
+from django.conf import settings
 
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+from products.models import Product
+from bag.contexts import bag_contents
+
+import os
+from dotenv import load_dotenv
+
+import stripe
+import json
+
+load_dotenv()
+
+stripe_public_key = os.getenv('STRIPE_PUBLIC_KEY')
+stripe_secret_key = os.getenv('STRIPE_SECRET_KEY')
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = stripe_secret_key
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
+    print("asfered")
 
-    load_dotenv()
-
-    stripe_public_key = os.getenv('STRIPE_PUBLIC_KEY')
-    stripe_secret_key = os.getenv('STRIPE_SECRET_KEY')
-
-    if request.method == "POST":
-
+    if request.method == 'POST':
         bag = request.session.get('bag', {})
 
         form_data = {
@@ -33,9 +54,12 @@ def checkout(request):
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
-
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -62,45 +86,34 @@ def checkout(request):
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
-            
+
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
     else:
-
         bag = request.session.get('bag', {})
         if not bag:
             messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
-        
+
         current_bag = bag_contents(request)
         total = current_bag['grand_total']
         stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
         order_form = OrderForm()
 
-    if not stripe_public_key or not stripe_secret_key:
-        messages.error(request, "Stripe API keys are not set.")
-        return redirect(reverse('products'))
-
-    stripe.api_key = stripe_secret_key
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
-
-    try:
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,  # Amount in cents
-            currency='usd',
-            # Verify your integration in this guide by including this parameter
-            metadata={'integration_check': 'accept_a_payment'},
-        )
-    except stripe.error.StripeError as e:
-        # Handle Stripe errors, if any
-        messages.error(request, str(e))
-        return redirect(reverse('products'))
-
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
@@ -108,6 +121,7 @@ def checkout(request):
     }
 
     return render(request, template, context)
+
 
 def checkout_success(request, order_number):
     """
