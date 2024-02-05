303cdbbea8f19afd92df823bd16a15d843d63c87
Chris <christopherryan91@gmail.com>
2024-02-05 15:51:49 +0000

    Added profile form, views and updated template for orders, removed success redirect, fixed type on Order model

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
 from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
 
@@ -9,9 +12,19 @@ def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form= UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile Updated Succesfully')
+
+    form = UserProfileForm(instance=profile)
     template = 'profiles/profile.html'
+    orders = profile.orders.all()
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
