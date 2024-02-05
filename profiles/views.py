a5c49e9097d0e88139a8f2d5c9eff8552c1f48bc
Chris <christopherryan91@gmail.com>
2024-02-05 22:06:25 +0000

    Secured the views

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
@@ -9,7 +9,7 @@ from checkout.models import Order
 # Create your views here.
 
 
-
+@login_required
 def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -35,6 +35,7 @@ def profile(request):
 
     return render(request, template, context)
 
+@login_required
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
