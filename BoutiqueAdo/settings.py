297342e6dba409d3e8f0ccad7dfdc18511db152a
Chris <christopherryan91@gmail.com>
2024-02-06 13:21:43 +0000

    Depoyment commit

--- BoutiqueAdo/settings.py
+++ BoutiqueAdo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -32,7 +33,7 @@ SECRET_KEY = 'django-insecure-kda5$$ncqqs3kw!@l1z02(dq_acx3z9k0*$8@fqm_m)&yr)qx_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bout-a.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,13 +121,19 @@ WSGI_APPLICATION = 'BoutiqueAdo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
 
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
