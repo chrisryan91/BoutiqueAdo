54aa18de467a3f2b6462430998de8fe1484e3846
Chris <christopherryan91@gmail.com>
2024-02-07 22:38:41 +0000

    Add new credentials and new bucket

--- BoutiqueAdo/settings.py
+++ BoutiqueAdo/settings.py
@@ -180,7 +180,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'bouti-ad'
+    AWS_STORAGE_BUCKET_NAME = 'bout-aa'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
