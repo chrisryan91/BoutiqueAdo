5deb422ae5a90d2ee673f10c4c52b5597d3dd3e7
Chris <christopherryan91@gmail.com>
2024-02-10 14:42:27 +0000

    Add AWS keys to env file

--- BoutiqueAdo/settings.py
+++ BoutiqueAdo/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bout-a-86a314738803.herokuapp.com', 'localhost', '127.0.0.1']
 
